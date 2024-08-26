8b4b3c99c130520d6d93664213e6f027c1810904
Aiden Cullo <aidencullo@gmail.com>
2024-08-26 14:11:48 -0400

    define promise beforehand

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-// As an additional requirement, note that each database request takes 100ms to respond. However, your function must complete in 200ms or less. Since there are three different databases, you must query; one might assume that the minimum time to do so would be 300ms, but that is not the case.
-
-
 /**
  * Retrieves user data from multiple databases.
  *
@@ -14,36 +11,45 @@ async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
-    db3: db3
+    db3: db3,
   };
 
   try {
-    // myFunction: 429.136962890625 ms
-    const [personalData, secretPersonalData] = await Promise.all([
-      central(id).then(db => dbs[db](id)),
-      vault(id)
-    ]);
+    // Start both database queries in parallel.
+    const dbPromise = central(id);
+    const vaultPromise = vault(id);
 
-    // myFunction: 1339.989990234375 ms
-    // const db = await central(id);
-    // const personalData = await dbs[db](id);
-    // const secretPersonalData = await vault(id);
+    // Wait for central to resolve and determine the correct database.
+    const dbName = await dbPromise;
+    const personalDataPromise = dbs[dbName](id);
+
+    // Wait for all promises to resolve.
+    const [personalData, secretPersonalData] = await Promise.all([
+      personalDataPromise,
+      vaultPromise,
+    ]);
 
     return {
       id,
       ...personalData,
-      ...secretPersonalData
+      ...secretPersonalData,
     };
   } catch (error) {
     console.error(`Error retrieving data for user ${id}:`, error);
-    throw error;  // Re-throw the error after logging it
+    throw new Error(`Failed to retrieve user data for ID ${id}`);
   }
 }
 
 async function timedFunction() {
   console.time('myFunction');
-  const response = await getUserData(1)
-  console.timeEnd('myFunction');
+  try {
+    const response = await getUserData(1);
+    console.log(response); // You can remove or modify this line as needed.
+  } catch (error) {
+    console.error(error.message);
+  } finally {
+    console.timeEnd('myFunction');
+  }
 }
 
 timedFunction();
