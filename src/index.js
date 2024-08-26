b5a98bcc348453cbcebddc33cdf7404b52219524
Aiden Cullo <aidencullo@gmail.com>
2024-08-26 13:40:30 -0400

    part 1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 // Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-function getUserData(id) {
+async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3
   };
 
-  
+  const db = await central(id);
+  const person = await dbs[db](id);
+  const secretPerson = await vault(id);
+  console.log(person);
+  console.log(secretPerson);
+  return person;
 }
+
+getUserData(1);
 
 
 // sample output obj
