66e6c7fb4ddb82d62a52dc581565d30c5ab65ec8
Aiden Cullo <aidencullo@gmail.com>
2024-08-26 14:13:27 -0400

    simpolify

--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-// Importing database functions. DO NOT MODIFY THIS LINE.
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-/**
- * Retrieves user data from multiple databases.
- *
- * @param {number} id - The ID of the user.
- * @returns {Promise<Object>} A promise that resolves to an object containing the user's data.
- */
 async function getUserData(id) {
   const dbs = {
     db1: db1,
@@ -15,15 +8,12 @@ async function getUserData(id) {
   };
 
   try {
-    // Start both database queries in parallel.
     const dbPromise = central(id);
     const vaultPromise = vault(id);
 
-    // Wait for central to resolve and determine the correct database.
     const dbName = await dbPromise;
     const personalDataPromise = dbs[dbName](id);
 
-    // Wait for all promises to resolve.
     const [personalData, secretPersonalData] = await Promise.all([
       personalDataPromise,
       vaultPromise,
@@ -40,16 +30,7 @@ async function getUserData(id) {
   }
 }
 
-async function timedFunction() {
-  console.time('myFunction');
-  try {
-    const response = await getUserData(1);
-    console.log(response); // You can remove or modify this line as needed.
-  } catch (error) {
-    console.error(error.message);
-  } finally {
-    console.timeEnd('myFunction');
-  }
-}
 
-timedFunction();
+getUserData(1)
+  .then(console.log)
+  .catch(console.error);
