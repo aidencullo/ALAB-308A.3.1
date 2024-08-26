f6ce4130a68f185bb2246b796f529e60eea5b114
Aiden Cullo <aidencullo@gmail.com>
2024-08-26 11:06:17 -0400

    setup sampel return obj

--- src/index.js
+++ src/index.js
@@ -7,4 +7,32 @@ function getUserData(id) {
     db2: db2,
     db3: db3
   };
+
+  
 }
+
+
+// sample output obj
+// {
+//     id: number,
+//     name: string,
+//     username: string,
+//     email: string,
+//     address: {
+//       street: string,
+//       suite: string,
+//       city: string,
+//       zipcode: string,
+//       geo: {
+//         lat: string,
+//         lng: string
+//       }
+//     },
+//     phone: string,
+//     website: string,
+//     company: {
+//       name: string,
+//       catchPhrase: string,
+//       bs: string
+//     }
+// }
