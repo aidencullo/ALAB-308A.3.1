1f2fb4e7adb58183126c38f34805a460f30743d5
Aiden Cullo <aidencullo@gmail.com>
2024-08-27 08:49:40 -0400

    test cases on invalid id

--- src/index.js
+++ src/index.js
@@ -36,23 +36,32 @@ async function testSuite() {
 
   try {
     const userData = await getUserData(11);
-    console.log(`User data for IDI11:`, userData);
   } catch (error) {
     console.error(`Error retrieving data for user 11:`, error);
   }
 
   try {
     const userData = await getUserData(12);
-    console.log(`User data for IDI12:`, userData);
   } catch (error) {
     console.error(`Error retrieving data for user 12:`, error);
   }
 
   try {
     const userData = await getUserData(0);
-    console.log(`User data for IDI0:`, userData);
   } catch (error) {
     console.error(`Error retrieving data for user 0:`, error);
+  }
+
+  try {
+    const userData = await getUserData(true);
+  } catch (error) {
+    console.error(`Error retrieving data for user true:`, error);
+  }
+
+  try {
+    const userData = await getUserData('');    
+  } catch (error) {
+    console.error(`Error retrieving data for user '':`, error);
   }
 
 }
