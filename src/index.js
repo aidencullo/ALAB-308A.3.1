88120bc0eb6623d99d1bcd8de4a657134e958e87
Aiden Cullo <aidencullo@gmail.com>
2024-08-27 08:52:03 -0400

    move test suite to separate file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { central, db1, db2, db3, vault } from "./databases.js";
+import { testSuite } from "./tests.js";
 
 async function getUserData(id) {
   const dbs = {
@@ -23,47 +24,4 @@ async function getUserData(id) {
   };
 }
 
-async function testSuite() {
-
-  Array.from({ length: 10 }, (_, i) => i + 1).map(async (i) => {
-    try {
-      const userData = await getUserData(i);
-      console.log(`User data for IDI${i}:`, userData);
-    } catch (error) {
-      console.error(`Error retrieving data for user ${i}:`, error);
-    }
-  });
-
-  try {
-    const userData = await getUserData(11);
-  } catch (error) {
-    console.error(`Error retrieving data for user 11:`, error);
-  }
-
-  try {
-    const userData = await getUserData(12);
-  } catch (error) {
-    console.error(`Error retrieving data for user 12:`, error);
-  }
-
-  try {
-    const userData = await getUserData(0);
-  } catch (error) {
-    console.error(`Error retrieving data for user 0:`, error);
-  }
-
-  try {
-    const userData = await getUserData(true);
-  } catch (error) {
-    console.error(`Error retrieving data for user true:`, error);
-  }
-
-  try {
-    const userData = await getUserData('');    
-  } catch (error) {
-    console.error(`Error retrieving data for user '':`, error);
-  }
-
-}
-
-testSuite();
+testSuite(getUserData);
